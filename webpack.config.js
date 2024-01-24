e0afddbf5f1879cf55b8a339891b506b65b3b4d3
Mahdi Torkaman <damavandon@gmail.com>
2024-01-24 13:50:43 +0330

    configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'postcss-loader'],
+      },
     ],
   },
   resolve: {
